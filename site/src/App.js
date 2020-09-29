37f5dcb0b95f3417395ecb9a253a9f1d5cca58a2
Heng Zhao <zhaoh@opentext.com>
2020-09-29 13:15:48 -0400

    button draft

--- site/src/App.js
+++ site/src/App.js
@@ -4,6 +4,22 @@ function App() {
   return (
    <div>
      <p className="badge">test</p>
+     <button className="button">
+        submit
+     </button>
+     <button className="button1">
+        submit1
+     </button>
+     
+     <button className="button2">
+        submit1
+     </button>
+     <button className="button3">
+        submit1
+     </button>
+     <button className="button4">
+        submit1
+     </button>
    </div>
   );
 }
