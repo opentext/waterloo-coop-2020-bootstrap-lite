cd5be77b86f8861a6057f163690f19c70f8cec06
Saqib <saqibalip321@gmail.com>
2020-09-29 13:13:37 -0400

    added badge

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "stylelint-config-standard",
+    "plugins": ["stylelint-scss"],
+    "rules": {
+      "at-rule-no-unknown": null,
+      "scss/at-rule-no-unknown": true,
+      "no-descending-specificity": null,
+      "no-missing-end-of-source-newline": null
+    },
+  }
