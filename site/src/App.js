93d424ca2a020bc06b775845993ce0dbd0f14b8c
Saqib <saqibalip321@gmail.com>
2020-09-29 13:56:45 -0400

    .gitignore fix

--- site/src/App.js
+++ site/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import "./bootstrap-lite.css"
 function App() {
   return (
    <div>
@@ -42,7 +41,7 @@ function App() {
         <p>This will be main content pane with examples of components</p>
         
         <div>
-          <h1>Badge</h1>
+          <h1>Badge <span className = "badge">new</span></h1>
           <h4>Description of component</h4>
           <h3>How it works</h3>
           <p>Some info on how to use</p>
