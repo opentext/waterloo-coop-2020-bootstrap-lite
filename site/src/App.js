1c4253cdb7b965337f191faed1eb2e2072f78b72
Rntrtul <Sistthelord@gmail.com>
2020-09-30 13:52:41 -0400

    add code snippets, grid, container

--- site/src/App.js
+++ site/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Highlight from 'react-highlight.js'
+
 function App() {
+  
+  let badge_code = `<h1>Badge <span className = "badge badge-warning">New</span></h1>`
+  let buttons_code = `<h1>REPLACE ME WITH CODE</h1>`
+  let card_code = `<div className = "card" style="width: 36rem">
+  <div className = "card-header"> Header </div>
+  <img src = "smallPic.png" className = "card-img" alt="ring" style= "height: "auto", width: 256 + "px"}}/>
+  <div className = "card-body">
+    <h3 className= "card-title">Empty Circle</h3>
+    <p className = "card-subtitle">Ring</p>
+    <p className = "card-text">Some text</p>
+    <p className = "card-text">More text</p>
+  </div>
+  <ul className = "list-group list-group-flush">
+    <li className = "list-group-item">Item 1</li>
+    <li className = "list-group-item">Item 2</li>
+    <li className = "list-group-item">Item 3</li>
+    <li className = "list-group-item">Item 4</li>
+  </ul>
+  <div className = "card-footer"> Footer </div>
+</div>`
+  let media_object_code = `<h1>REPLACE ME WITH CODE</h1>`
+  let navbar_code = `<h1>REPLACE ME WITH CODE</h1>`
+
+
   return (
-   <div>
-     <nav>
+  <div>
+    <nav>
        <ul>
          <li>
            <a href="#">Home</a>
@@ -16,103 +42,109 @@ function App() {
           </li>
        </ul>       
      </nav>
-     <div>
-      <div>
-        <ul>
-          <li>
-            <a href="#">Badge</a>
-            </li>
-            <li>
-            <a href="#">Buttons</a>
-            </li>
-            <li>
-            <a href="#">Cards</a>
-            </li>
-            <li>
-            <a href="#">Media Object</a>
-            </li>
-            <li>
-            <a href="#">Navbar</a>
-            </li>
-        </ul>
-        <p>This will be a scrollable thing on left side of screen</p>
-      </div>
-      
-      <div>
-        <p>This will be main content pane with examples of components</p>
-        
-        <div>
-          <h1>Badge <span className = "badge badge-warning">new</span></h1>
-          <h4>Description of component</h4>
-          <h3>How it works</h3>
-          <p>Some info on how to use</p>
-          <p>example</p>
-          <pre>
-            <code>Code used to create above example</code>
-          </pre>
-        </div>
+    <div>
+      <div className = "container">
+        <div className = "row">
+          <div className = "col-2 sidebar" style={{"background-color": "azure"}}>
+            <ul>
+              <li>
+                <a href="#Badge">Badge</a>
+                </li>
+                <li>
+                <a href="#Buttons">Buttons</a>
+                </li>
+                <li>
+                <a href="#Card">Card</a>
+                </li>
+                <li>
+                <a href="#Media-Object">Media Object</a>
+                </li>
+                <li>
+                <a href="#Navbar">Navbar</a>
+                </li>
+            </ul>
+          </div>
 
-        <div>
-          <h1>Buttons</h1>
-          <h4>Description of component</h4>
-          <h3>How it works</h3>
-          <p>Some info on how to use</p>
-          <p>example</p>
-          <pre>
-            <code>Code used to create above example</code>
-          </pre>
-        </div>
+          <div className = "col" style={{"background-color": "beige"}}>
+            
+            <div id="Badge">
+              <h1>Badge <span className = "badge badge-warning">New</span></h1>
+              <h4>Description of component</h4>
+              <h3>How it works</h3>
+              <p>Some info on how to use</p>
+              <p>example</p>
+              <Highlight>
+                {badge_code}
+              </Highlight>
+            </div>
 
-        <div>
-          <h1>Cards</h1>
-          <h4>Description of component</h4>
-          <h3>How it works</h3>
-          <p>Some info on how to use</p>
-          <p>example</p>
-          <div className = "card">
-            <div className = "card-header">
-              Header
+            <div id="Buttons">
+              <h1>Buttons</h1>
+              <h4>Description of component</h4>
+              <h3>How it works</h3>
+              <p>Some info on how to use</p>
+              <p>example</p>
+              <Highlight>
+                {buttons_code}
+              </Highlight>
             </div>
-            <img src = "..." className = "card-img" alt="react logo"/>
-            <div className = "card-body">
-              <p className= "card-title">Some sample text in card</p>
-              <p className = "card-subtitle">Subttile</p>
-              <p className = "card-text">Some text</p>
-              <p className = "card-text">More text</p>
+
+            <div id="Card">
+              <h1>Card</h1>
+              <h4>A content container with multiple options and wide variety of uses</h4>
+              <h3>How it works</h3>
+              <p>Some info on how to use</p>
+              <p>example</p>
+              <div className = "card" style={{width: 36 + 'rem'}}>
+                <div className = "card-header"> Header </div>
+                <img src = "smallPic.png" className = "card-img" alt="ring" style= {{height: "auto", width: 256 + "px"}}/>
+                <div className = "card-body">
+                  <h3 className= "card-title">Empty Circle</h3>
+                  <p className = "card-subtitle">Ring</p>
+                  <p className = "card-text">Some text</p>
+                  <p className = "card-text">More text</p>
+                </div>
+                <ul className = "list-group list-group-flush">
+                  <li className = "list-group-item">Item 1</li>
+                  <li className = "list-group-item">Item 2</li>
+                  <li className = "list-group-item">Item 3</li>
+                  <li className = "list-group-item">Item 4</li>
+                </ul>
+                <div className = "card-footer"> Footer </div>
+              </div>
+              <h4>Code</h4>
+              <Highlight language = {"html"}>
+                {card_code}
+              </Highlight>
             </div>
-            <div className = "card-footer">
-              Footer
+
+            <div id = "Media-Object">
+              <h1>Media Object</h1>
+              <h4>Description of component</h4>
+              <h3>How it works</h3>
+              <p>Some info on how to use</p>
+              <p>example</p>
+              <Highlight>
+                {media_object_code}
+              </Highlight>
+            </div>
+
+            <div id="Navbar">
+              <h1>Navbar</h1>
+              <h4>Description of component</h4>
+              <h3>How it works</h3>
+              <p>Some info on how to use</p>
+              <p>example</p>
+              <Highlight>
+                {navbar_code}
+              </Highlight>
             </div>
           </div>
-          <pre>
-            <code>Code used to create above example</code>
-          </pre>
-        </div>
 
-        <div>
-          <h1>Media Object</h1>
-          <h4>Description of component</h4>
-          <h3>How it works</h3>
-          <p>Some info on how to use</p>
-          <p>example</p>
-          <pre>
-            <code>Code used to create above example</code>
-          </pre>
-        </div>
-
-        <div>
-          <h1>Navbar</h1>
-          <h4>Description of component</h4>
-          <h3>How it works</h3>
-          <p>Some info on how to use</p>
-          <p>example</p>
-          <pre>
-            <code>Code used to create above example</code>
-          </pre>
         </div>
       </div>
-     </div>
-   </div>
+    </div>
+  </div>
   );
 }
 
