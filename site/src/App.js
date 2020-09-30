cf725e5c25b6dd05c4be134900b53f8fbd8d005c
Rntrtul <Sistthelord@gmail.com>
2020-09-30 16:44:45 -0400

    typography, re-arrange elements

--- site/src/App.js
+++ site/src/App.js
@@ -53,9 +53,9 @@ function App() {
      </nav>
 
     <div className = "container">
-        <div className = "row">
-          
-          <div className = "col-2 sidebar" style={{"background-color": "azure"}}>
+      <div className = "row">
+        <div className = "col-2">
+          <div className = "col-10 sidebar" >
             <ul>
               <li>
                 <a href="#Badge">Badge</a>
@@ -74,106 +74,107 @@ function App() {
                 </li>
             </ul>
           </div>
+        </div>
 
-          <div className = "col" style={{"background-color": "beige"}}>
-            
-            <div id="Badge">
-              <h1>Badge <span className = "badge badge-warning">New</span></h1>
-              <h4>Description of component</h4>
-              <h3>How it works</h3>
-              <p>Some info on how to use</p>
-              <p>example</p>
-              <span className = "badge badge-primary">primary</span>
-              <span className = "badge badge-success">success</span>
-              <span className = "badge badge-warning">warning</span>
-              <Highlight>
-                {badge_code}
-              </Highlight>
-            </div>
-
-            <div id="Buttons">
-              <h1>Buttons</h1>
-              <h4>Description of component</h4>
-              <h3>How it works</h3>
-              <p>Some info on how to use</p>
-              <p>example</p>
-              <div>
-                <button className="button">
-                  submit
-                </button>
-                <button className="button1">
-                  submit
-                </button>
-                <button className="button2">
-                  submit
-                </button>
-                <button className="button3">
-                  submit
-                </button>
-                <button className="button4">
-                  submit
-                </button>
-              </div>
-              <Highlight>
-                {buttons_code}
-              </Highlight>
-            </div>
-
-            <div id="Card">
-              <h1>Card</h1>
-              <h4>A content container with multiple options and wide variety of uses</h4>
-              <h3>How it works</h3>
-              <p>Some info on how to use</p>
-              <p>example</p>
-              <div className = "card" style={{width: 36 + 'rem'}}>
-                <div className = "card-header"> Header </div>
-                <img src = "smallPic.png" className = "card-img" alt="ring" style= {{height: "auto", width: 256 + "px"}}/>
-                <div className = "card-body">
-                  <h3 className= "card-title">Empty Circle</h3>
-                  <p className = "card-subtitle">Ring</p>
-                  <p className = "card-text">Some text</p>
-                  <p className = "card-text">More text</p>
-                </div>
-                <ul className = "list-group list-group-flush">
-                  <li className = "list-group-item">Item 1</li>
-                  <li className = "list-group-item">Item 2</li>
-                  <li className = "list-group-item">Item 3</li>
-                  <li className = "list-group-item">Item 4</li>
-                </ul>
-                <div className = "card-footer"> Footer </div>
-              </div>
-              <h4>Code</h4>
-              <Highlight language = {"html"}>
-                {card_code}
-              </Highlight>
-            </div>
-
-            <div id = "Media-Object">
-              <h1>Media Object</h1>
-              <h4>Description of component</h4>
-              <h3>How it works</h3>
-              <p>Some info on how to use</p>
-              <p>example</p>
-              <Highlight>
-                {media_object_code}
-              </Highlight>
-            </div>
-
-            <div id="Navbar">
-              <h1>Navbar</h1>
-              <h4>Description of component</h4>
-              <h3>How it works</h3>
-              <p>Some info on how to use</p>
-              <p>example</p>
-              <Highlight>
-                {navbar_code}
-              </Highlight>
-            </div>
+        <div className = "col-8" >
+          
+          <div id="Badge">
+            <h1>Badge <span className = "badge badge-warning">New</span></h1>
+            <h4>Description of component</h4>
+            <h3>How it works</h3>
+            <p>Some info on how to use</p>
+            <p>example</p>
+            <span className = "badge badge-primary">primary</span>
+            <span className = "badge badge-success">success</span>
+            <span className = "badge badge-warning">warning</span>
+            <Highlight>
+              {badge_code}
+            </Highlight>
           </div>
 
-        </div>
-      </div>
+          <div id="Buttons">
+            <h1>Buttons</h1>
+            <h4>Description of component</h4>
+            <h3>How it works</h3>
+            <p>Some info on how to use</p>
+            <p>example</p>
+            <div>
+              <button className="button">
+                submit
+              </button>
+              <button className="button1">
+                submit
+              </button>
+              <button className="button2">
+                submit
+              </button>
+              <button className="button3">
+                submit
+              </button>
+              <button className="button4">
+                submit
+              </button>
+            </div>
+            <Highlight>
+              {buttons_code}
+            </Highlight>
+          </div>
 
+          <div id="Card">
+            <h1>Card</h1>
+            <h4>A content container with multiple options and wide variety of uses</h4>
+            <h3>How it works</h3>
+            <p>Some info on how to use</p>
+            <p>example</p>
+
+            <div className = "card" style={{width: 36 + 'rem'}}>
+              <div className = "card-header"> Header </div>
+              <img src = "smallPic.png" className = "card-img" alt="ring" style= {{height: "auto", width: 256 + "px"}}/>
+              <div className = "card-body">
+                <h3 className= "card-title">Empty Circle</h3>
+                <p className = "card-subtitle">Ring</p>
+                <p className = "card-text">Some text</p>
+                <p className = "card-text">More text</p>
+              </div>
+              <ul className = "list-group list-group-flush">
+                <li className = "list-group-item">Item 1</li>
+                <li className = "list-group-item">Item 2</li>
+                <li className = "list-group-item">Item 3</li>
+                <li className = "list-group-item">Item 4</li>
+              </ul>
+              <div className = "card-footer"> Footer </div>
+            </div>
+            <h4>Code</h4>
+            <Highlight language = {"html"}>
+              {card_code}
+            </Highlight>
+          </div>
+
+          <div id = "Media-Object">
+            <h1>Media Object</h1>
+            <h4>Description of component</h4>
+            <h3>How it works</h3>
+            <p>Some info on how to use</p>
+            <p>example</p>
+            <Highlight>
+              {media_object_code}
+            </Highlight>
+          </div>
+
+          <div id="Navbar">
+            <h1>Navbar</h1>
+            <h4>Description of component</h4>
+            <h3>How it works</h3>
+            <p>Some info on how to use</p>
+            <p>example</p>
+            <Highlight>
+              {navbar_code}
+            </Highlight>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   </div>
   );
 }
