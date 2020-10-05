7d9eaeb0703ec37d9e65ad27445cd0ab7c7e64e0
saqib <saqibalip321@gmail.com>
2020-10-05 17:34:12 -0400

    added spring initializer files

---
+++ site/ReactApp/src/main/java/com/example/ReactApp/ReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAppApplication.class, args);
+	}
+
+}
---
+++ site/ReactApp/src/test/java/com/example/ReactApp/ReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
