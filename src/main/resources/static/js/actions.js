0aaca90246a60cf807e5378884864661fd4b2bbc
fhussain@opentext.com <fareed@FHUSSAINDMVBHV1.opentext.net>
2020-10-08 12:55:31 -0400

    scss compiler integrated

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -1,8 +1,1 @@
-  var transparent= "navbar navbar-transparent navbar-fixed";
-  var primary= "navbar navbar-primary navbar-fixed";
-  var dark= "navbar navbar-dark navbar-fixed";
-  var light= "navbar navbar-light navbar-fixed";
-
- function changeNavbar(arg){
-    document.getElementById("navbarid").className = arg;
- }
+  let buttons_code = "heelo";
